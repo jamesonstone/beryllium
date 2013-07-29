5aa7193b39b789d30847bcb49b6e41f07d5ee52c
J.Stone <jameson@stone.tc>
2013-07-28 21:01:00 -0400

    adding login tests

---
+++ test_login.js
@@ -0,0 +1,76 @@
+/**
+ * tools: casperjs/phantomjs
+ *
+ * This test navigates to voxy.com/u/login/ 
+ * and tests the login function
+ *
+ * @author J.Stone
+ */
+var login_page = 'http://voxy.com/u/login/';
+var homepage = 'http://voxy.com/';
+var logout = 'http://voxy.com/u/logout/';
+
+var x = require('casper').selectXPath;
+var casper = require('casper').create({
+	//verbose: true, 
+	//logLevel: 'debug'
+});
+
+
+casper.test.begin("Test a valid login at /u/login/", 1, function(test) {
+	casper.start(login_page, function() {
+		this.fill('form#ajax-login-form', {
+		'username':    'voxytest@gmail.com',
+		'password':    'things'
+		}, true);
+	});
+
+	casper.then(function() {
+		var check_url = this.getCurrentUrl();
+		this.echo('current url: ' + check_url);
+
+		//check for the substring "guide/recommend/" in the url
+		if(check_url.indexOf("guide/recommend/") === -1) {
+			casper.test.fail('url does not contain guide/recommend/');
+		} else {
+			casper.test.pass('Guide is displayed');
+		}
+	});
+
+	casper.thenOpen(logout, function() {
+		//dump the current session and logout
+		//TODO: add this to teardown
+		this.echo('current url: ' + this.getCurrentUrl());
+	})
+
+	casper.run(function() {
+		test.done();
+	});
+
+});
+
+casper.test.begin("Test a invalid login at /u/login/", 1, function(test) {
+	casper.start(login_page, function() {
+		this.fill('form#ajax-login-form', {
+		'username':    'voxytest',
+		'password':    'things'
+		}, true);
+	});
+
+	casper.then(function() {
+		var check_url = this.getCurrentUrl();
+		this.echo('current url: ' + check_url);
+
+		//check for the substring "guide/recommend/" in the url
+		if(check_url.indexOf("guide/recommend/") === -1) {
+			casper.test.pass('url does not contain guide/recommend/');
+		} else {
+			casper.test.fail('Guide is displayed');
+		}
+	});
+
+	casper.run(function() {
+		test.done();
+	});
+
+});
