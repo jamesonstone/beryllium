07ec63086f0a166571ed231188eda06ed01d324e
Jameson <jameson@voxy.com>
2013-07-30 11:26:18 -0400

    added header login test and invalid header login tests

--- test_login.js
+++ test_login.js
@@ -27,7 +27,7 @@ casper.test.begin("Test a valid login at /u/login/", 1, function(test) {
 
 	casper.then(function() {
 		//added wait here because transition to guide wasn't occuring fast enough
-		this.wait(2000, function() {
+		this.wait(1000, function() {
 			var check_url = this.getCurrentUrl();
 			this.echo('current url: ' + check_url);
 
@@ -46,6 +46,7 @@ casper.test.begin("Test a valid login at /u/login/", 1, function(test) {
 		//dump the current session and logout
 		//TODO: add this to teardown
 		this.echo('current url: ' + this.getCurrentUrl());
+		this.wait(500);
 	});
 
 	casper.run(function() {
@@ -83,7 +84,85 @@ casper.test.begin("Test a invalid login at /u/login/", 2, function(test) {
 		//dump the current session and logout
 		//TODO: add this to teardown		
 		this.echo('current url: ' + this.getCurrentUrl());
+		this.wait(500);		
 	});
+
+	casper.run(function() {
+		test.done();
+	});
+
+});
+
+
+casper.test.begin("Test a valid login at voxy.com (header)", 1, function(test) {
+	casper.start(homepage, function() {
+		this.click(x('//*[@id="header_login_Btn"]'));
+	});
+
+	casper.then(function() {
+		//fill out and submit login form
+		this.fill('form#header_login', {
+			'username':    'u1@voxy.com',
+			'password':    'things'
+		}, true);
+	});
+
+	casper.wait(1500, function() {
+		var check_url = this.getCurrentUrl();
+		this.echo('current url: ' + check_url);
+
+		//check for the substring "guide/recommend/" in the url
+		if(check_url.indexOf("guide/recommend/") === -1) {
+			casper.test.fail('url does not contain guide/recommend/');
+		} else {
+			casper.test.pass('Guide is displayed');
+		}
+	});
+
+	casper.thenOpen(logout, function() {
+		//dump the current session and logout
+		//TODO: add this to teardown		
+		this.echo('current url: ' + this.getCurrentUrl());
+		this.wait(500);		
+	});	
+
+	casper.run(function() {
+		test.done();
+	});
+
+});
+
+casper.test.begin("Test a invalid login at voxy.com (header)", 1, function(test) {
+	casper.start(homepage, function() {
+		this.click(x('//*[@id="header_login_Btn"]'));
+	});
+
+	casper.then(function() {
+		//fill out and submit login form
+		this.fill('form#header_login', {
+			'username':    'u1',
+			'password':    'things'
+		}, true);
+	});
+
+	casper.wait(1000, function() {
+		var check_url = this.getCurrentUrl();
+		this.echo('current url: ' + check_url);
+
+		//check for the substring "/u/login/" in the url (after redirect)
+		if(check_url.indexOf("u/login/") === -1) {
+			casper.test.fail('url does not contain u/login/');
+		} else {
+			casper.test.pass('User was redirected to u/login/ successfully');
+		}
+	});
+
+	casper.thenOpen(logout, function() {
+		//dump the current session and logout
+		//TODO: add this to teardown		
+		this.echo('current url: ' + this.getCurrentUrl());
+		this.wait(500);		
+	});	
 
 	casper.run(function() {
 		test.done();
@@ -92,4 +171,3 @@ casper.test.begin("Test a invalid login at /u/login/", 2, function(test) {
 	});
 
 });
-
