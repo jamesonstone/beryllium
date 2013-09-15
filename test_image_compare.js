0a991f99fc2cb69bc137141a5f00cc623a1f1c92
J.Stone <jameson@stone.tc>
2013-09-15 18:04:08 -0400

    adding script to screen-shot landing pages

--- test_image_compare.js
+++ test_image_compare.js
@@ -2,6 +2,7 @@
 
 var login_page = 'http://www.voxy.com/u/login/';
 var logout = 'http://www.voxy.com/u/logout/';
+var landing1 = 'http://voxy.com/landing/web/en/0/9';
 
 var fs = require('fs');
 var casper = require('casper').create({
@@ -11,30 +12,42 @@ var casper = require('casper').create({
 	}	
 });
 
-casper.test.begin("Check Compare page", 1, function(test) {
-var compare_page = 'https://voxy.com/guide/compare/';
 
-	casper.start(login_page, function() {
-		this.fill('form#ajax-login-form', {
-		'username':    'imagetester1@voxy.com',
-		'password':    'things'
-		}, true);
+function get_title(arg1) {
+	var n = arg1.substring(24, 35);
+	var arr = n.split("");
+
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] == '/') {
+			arr[i] = '_';
+		}
+	}
+	return arr.join('');
+}
+
+
+
+casper.test.begin("Take a screen-shot", 1, function(test) {
+
+	casper.start(landing1, function() {
+		this.wait(1000);
+		this.echo('current url: ' + this.getCurrentUrl());
 	});
 
-	casper.thenOpen(compare_page, function() {
-		this.wait(5000);
-		this.echo('current url: ' + this.getCurrentUrl());		
-	});
+/*	casper.thenOpen(compare_page, function() {
+		
+	});*/
 
 	casper.then(function() {
-		var ref_image = fs.read('./compare_page.png');
+		/*var ref_image = fs.read('./compare_page.png');*/
+
+		var t = get_title(landing1);
+
+		this.echo(t);		
+		this.capture('landing/' + t + '.png');
 		
-		this.capture('realtime_compare_page.png');
-		//this.capture('compage_page.png');
-		//this.echo('image captured');
-		
-		var new_image = fs.read('./realtime_compare_page.png');
-		this.test.assert(new_image == ref_image);
+/*		var new_image = fs.read('./realtime_compare_page.png');
+		this.test.assert(new_image == ref_image);*/
 	});
 
 	casper.thenOpen(logout, function() {
