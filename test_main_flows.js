de56573fbcd0f9a91a7466369cc0fc127cd192f3
Jameson <jameson@voxy.com>
2013-09-27 16:56:23 -0400

    added test_stage0 registration flow

--- test_main_flows.js
+++ test_main_flows.js
@@ -2,10 +2,8 @@
  * tools: casperjs/phantomjs
  *
  * Test the following for major elements:
- * >> Guide
- * >> Login
- * >> Registration
- * >> FUE Compare
+ * >> Guide // Login
+ * >> Registration // FUE Compare
  * >> Activity Workspace
  * >> Compare page
  * >> Tutoring Compare
@@ -165,6 +163,51 @@ casper.test.begin("Check that /guide/tutor/credits/ displays", 7, function(test)
 
 	casper.run(function() {
 		test.done();
+		//this.exit();
+	});	
+});
+
+
+casper.test.begin("Verify ability to register new user", 7, function(test) {
+	casper.Open(homepage, function() {
+		//wait for homepage to load
+		this.wait(1000, function() {
+			this.echo('current url: ' + this.getCurrentUrl());
+		});
+	});
+
+	casper.then(function() {
+		this.click('#fixed-login > div > div > div > a');
+		this.wait(1000);
+		this.echo('current url: ' + this.getCurrentUrl());
+	})
+
+	casper.then(function() {
+		this.wait(1000, function() {
+			test.assertExists('#main-content > div > div:nth-of-type(1) > div:nth-of-type(1)',
+			 'Tutor Compare header displays');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div',
+			 '1 Credit card is displayed');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div:nth-of-type(3) > a', 
+			 '1 Credit Buy button is displayed');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div', 
+			 '8 Credit card is displayed');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div:nth-of-type(3) > a', 
+			 '8 Credit Buy button is displayed');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(3) > div > div:nth-of-type(2) > div', 
+			 '20 Credit card is displayed');
+			test.assertExists('#main-content > div > div:nth-of-type(2) > div > div:nth-of-type(3) > div > div:nth-of-type(3) > a',
+			 '20 Credit Buy button is displayed');
+		});
+	});
+
+	casper.run(function() {
+		test.done();
 		this.exit();
 	});	
 });
+
+
+
+
+
--- test_stage0.js
+++ test_stage0.js
@@ -7,7 +7,7 @@
  *
  * @author J.Stone
  */
-
+//casperjs --ignore-ssl-errors=true test test_main_flows.js --foo=master
 // var login_page = 'http://master.pub.voxy.com/u/login/';
 // var logout = 'http://master.pub.voxy.com/u/logout/';
 
@@ -23,25 +23,76 @@ var casper = require('casper').create({
 
 
 var login_page = 'http://' + foo + '.pub.voxy.com/u/login/';
+var homepage = 'http://' + foo + '.pub.voxy.com/';
 var logout = 'http://' + foo + '.pub.voxy.com/u/logout/';
+var compare = 'http://' + foo + '.pub.voxy.com/guide/compare/';
+var tutor_compare = 'http://' + foo + '.pub.voxy.com/guide/tutor/credits/';
 
-casper.test.begin("Check the Guide displays", 0, function(test) {
-	casper.start(login_page, function() {
-		this.fill('form#ajax-login-form', {
-		'username':    'newu1@voxy.com',
-		'password':    'things'
-		}, true);
+casper.test.begin("Verify ability to register new user", 7, function(test) {
+	casper.start(homepage, function() {
+		this.wait(1000);
+		this.click('#fixed-login > div > div > div > a');
 	});
 
 	casper.then(function() {
 		this.wait(1000);
-		this.echo('url: ' + this.getCurrentUrl());
+		this.echo('current url: ' + this.getCurrentUrl());
+		test.assertExists('#main > div > div:nth-of-type(1) > div > div > div:nth-of-type(1)', 'Your Proficiency Level title displays');
+		test.assertExists('#main > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div > div > div:nth-of-type(6) > div > div > img', 
+			'Voxy logo displays in personalize pop up');
+		this.click('#main > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div > div > div:nth-of-type(6) > div > div > div > a');
+		test.assertExists('#level_1 > a', 'Level 1 icon displays');
+		test.assertExists('#level_2 > a', 'Level 2 icons displays');
+		test.assertExists('#level_3 > a', 'Level 3 icons displays');
+		test.assertExists('#level_4 > a', 'Level 4 icons displays');
+		test.assertExists('#level_5 > a', 'Level 5 icons displays');
+		test.assertExists('#level_6 > a', 'Level 6 icons displays');
+		test.assertExists('#level_7 > a', 'Level 7 icons displays');
 	});
+
+	casper.then(function() {
+		this.click('#level_1 > a');
+		this.wait(1000);
+		this.click('#main > div > div:nth-of-type(3) > div > div > div > div > button');
+	});
+
+	casper.then(function() {
+		this.wait(1000);
+		this.echo('current url: ' + this.getCurrentUrl());
+		test.assertExists('#main > div > div:nth-of-type(1) > div > div > div:nth-of-type(1)', 'Your Goals title displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(1) > div:nth-of-type(2)', 'Advance my Career displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(2) > div:nth-of-type(2)', 'Enjoy English Media displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(3) > div:nth-of-type(2)', 'Pass My English Test displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(4) > div:nth-of-type(2)', 'Travel Abroad displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(5) > div:nth-of-type(2)', 'Day to Day Tasks displays');
+		test.assertExists('#goal-form > ul > li:nth-of-type(6) > div:nth-of-type(2)', 'Social & Lifestyle');
+	});
+
+	casper.then(function() {
+		this.click('#goal-form > ul > li:nth-of-type(1) > div:nth-of-type(2)');
+		this.wait(1000);
+		this.click('#main > div > div:nth-of-type(3) > div > div > div > div > button');
+	});
+
+	casper.then(function() {
+		this.wait(1000);
+		this.echo('current url: ' + this.getCurrentUrl());
+
+		//add verifications for 3rd step of FUE
+
+	});
+
+
+
+
+
+
+
 
 	casper.thenOpen(logout, function() {
 		//dump the current session and logout
-		this.wait(500);
-		this.echo('url: ' + this.getCurrentUrl());		
+		this.echo('current url: ' + this.getCurrentUrl());
+
 	});
 
 	casper.run(function() {
